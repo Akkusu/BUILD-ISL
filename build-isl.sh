3cadbc9cfd78230d110ae750e0659ac6bf17f665
Akkusu <95112354+Akkusu@users.noreply.github.com>
2022-07-16 00:39:04 -0500

    Create build-isl.sh

---
+++ build-isl.sh
@@ -0,0 +1,13 @@
+echo "[Step 1.5] Build ISL..."
+  cd ${DOWNLOADDIR}
+  if [ ! -f isl-${ISL_VERSION}.tar.xz ]; then
+	curl -L -O http://isl.gforge.inria.fr/isl-${ISL_VERSION}.tar.xz
+  fi
+  tar xJf isl-${ISL_VERSION}.tar.xz -C ${SRCDIR}
+  rm -rf ${BUILDDIR}/isl-${ISL_VERSION}
+  mkdir -p ${BUILDDIR}/isl-${ISL_VERSION}
+  cd ${BUILDDIR}/isl-${ISL_VERSION}
+  ../${SRCRELDIR}/isl-${ISL_VERSION}/configure --build=${HOST_NATIVE} --host=${HOST_NATIVE} --prefix=${INSTALLDIR} --disable-shared --with-gmp-prefix=${INSTALLDIR}
+  make ${JOBS}
+  make install
+fi
